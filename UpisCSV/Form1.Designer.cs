163277d560eba1b99e0e388b47084be9a2af3b08
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-03-08 09:15:39 +0100

    Add files via upload

---
+++ UpisCSV/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace UpisCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtGodRod = new System.Windows.Forms.TextBox();
+            this.btnUnos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(166, 52);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(174, 31);
+            this.txtIme.TabIndex = 0;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(166, 110);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(174, 31);
+            this.txtPrezime.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(166, 168);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(174, 31);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtGodRod
+            // 
+            this.txtGodRod.Location = new System.Drawing.Point(166, 228);
+            this.txtGodRod.Name = "txtGodRod";
+            this.txtGodRod.Size = new System.Drawing.Size(174, 31);
+            this.txtGodRod.TabIndex = 3;
+            // 
+            // btnUnos
+            // 
+            this.btnUnos.Location = new System.Drawing.Point(182, 320);
+            this.btnUnos.Name = "btnUnos";
+            this.btnUnos.Size = new System.Drawing.Size(133, 67);
+            this.btnUnos.TabIndex = 4;
+            this.btnUnos.Text = "Unesi";
+            this.btnUnos.UseVisualStyleBackColor = true;
+            this.btnUnos.Click += new System.EventHandler(this.btnUnos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 474);
+            this.Controls.Add(this.btnUnos);
+            this.Controls.Add(this.txtGodRod);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtGodRod;
+        private System.Windows.Forms.Button btnUnos;
+    }
+}
+
---
+++ UpisCSV/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+
+namespace UpisCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnos_Click(object sender, EventArgs e)
+        {
+            List<Osoba> listOsoba = new List<Osoba>();
+            try
+            {
+                Osoba osoba = new Osoba(txtIme.Text, txtPrezime.Text, txtEmail.Text, Convert.ToInt32(txtGodRod.Text));
+
+                txtIme.Clear();
+                txtPrezime.Clear();
+                txtEmail.Clear();
+                txtGodRod.Clear();
+
+                DialogResult upit = MessageBox.Show("Zelite li unijeti jos podataka", "upit",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                switch (upit)
+                {
+                    case DialogResult.Yes:
+                        {
+                            listOsoba.Add(osoba);
+                            break;
+                        }
+                    case DialogResult.No:
+                        {
+                            listOsoba.Add(osoba);
+                            break;
+                        }
+
+                }
+            }catch(Exception greska)
+            {
+                MessageBox.Show(greska.Message, "Pogresan unos",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            using (var writer = new StreamWriter("filePersons.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(listOsoba);
+            }
+        }
+    }
+}
